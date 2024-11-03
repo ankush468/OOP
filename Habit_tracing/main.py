2ea205f42bbb0c6ce1b74dee12ae9fdb0e3d0806
Ankush Rai <ankushrai468@gmail.com>
2024-11-03 14:47:24 +0530

    base_url_chnage

--- Habit_tracing/main.py
+++ Habit_tracing/main.py
@@ -10,9 +10,9 @@ BASE_URL = 'https://pixe.la'
 
 ############################ create user account return a dict ############################ 
 
-user_creation_endpoint = '/v1/users'
+BASE_URL = '/v1/users'
 
-user_creation_url = f'{BASE_URL}{user_creation_endpoint}'
+user_creation_url = f'{BASE_URL}{BASE_URL}'
 
 print(user_creation_url)
 
