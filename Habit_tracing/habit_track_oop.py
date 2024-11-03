3d6d751e9a00c8b7a0c57b2dbb6f960b8683b276
Ankush Rai <ankushrai468@gmail.com>
2024-11-03 14:44:51 +0530

    Done with Pixel

--- Habit_tracing/habit_track_oop.py
+++ Habit_tracing/habit_track_oop.py
@@ -14,17 +14,25 @@ class Habit_tracking():
     
     def Create_graph(self, graph_creation_url, header_for_all,graph_creation_payload):
 
-        self.header_for_all = header_for_all
-
         response = request(method="POST",url=graph_creation_url, json=graph_creation_payload, headers=header_for_all)
         return response.json()
     
-    def Get_graph(self , get_graph_endpoint):
+    def Get_graph(self , get_graph_endpoint, header_for_all):
 
-        response = request(method="GET", url=get_graph_endpoint,headers=self.header_for_all)
+        response = request(method="GET", url=get_graph_endpoint,headers=header_for_all)
         return response.json()
     
-    def Create_pixel(self, post_pixel_endpoint, post_pixel_data):
+    def Create_pixel(self, post_pixel_endpoint, post_pixel_data , header_for_all):
 
-        response = request(method="POST",url=post_pixel_endpoint, headers=self.header_for_all , json=post_pixel_data)
+        response = request(method="POST",url=post_pixel_endpoint, headers=header_for_all , json=post_pixel_data)
+        return response.json()
+    
+    def update_pixel(self, put_graph_endpoint , put_pixel_chnage , header_for_all):
+
+        response = request(method="PUT", url=put_graph_endpoint, json=put_pixel_chnage , headers=header_for_all)
+        return response.json()
+    
+    def Delete_pixel(self, put_graph_endpoint , header_for_all):
+
+        response = request(method="DELETE", url=put_graph_endpoint, headers=header_for_all)
         return response.json()
--- Habit_tracing/main.py
+++ Habit_tracing/main.py
@@ -8,12 +8,7 @@ HABIT_TRACK_TOKEN = os.getenv('NEWS_API_KEY')
 BASE_URL = 'https://pixe.la'
 
 
-print(type(datetime.now().strftime("%Y%m%d")))
-
-
-
-
-# create user account return a dict
+############################ create user account return a dict ############################ 
 
 user_creation_endpoint = '/v1/users'
 
@@ -29,10 +24,10 @@ user_creation_payload = {
 }
 
 create_user = Habit_tracking(user_creation_url,user_creation_payload)
-# user = create_user.Create_user()
-# print(user)
+user = create_user.Create_user()
+print(user)
 
-# create a graph defination
+#############################  create a graph defination ############################ 
 
 graph_creation_endpoint = '/v1/users/'+HABIT_USER_NAME+'/graphs'
 
@@ -56,19 +51,19 @@ Create_graph = create_user.Create_graph(graph_creation_url, header_for_all,graph
 print(Create_graph)
 
 
-# get graph details
+#############################  get graph details ############################ 
 
 get_graph_endpoint = f'{BASE_URL}{graph_creation_endpoint}'
 
 print(get_graph_endpoint)
 
 
-# get_graph = create_user.Get_graph(get_graph_endpoint)
-# print(get_graph)
+get_graph = create_user.Get_graph(get_graph_endpoint , header_for_all)
+print(get_graph)
 
 
 
-# post a value on the graph
+#############################  post a value on the graph ############################ 
 
 post_pixel_endpoint = f'{BASE_URL}{graph_creation_endpoint}/{graph_creation_payload['id']}'
 
@@ -77,6 +72,26 @@ post_pixel_data = {
     'quantity' : '9.5',
 }
 
-create_pixel = create_user.Create_pixel(post_pixel_endpoint,post_pixel_data)
-
+create_pixel = create_user.Create_pixel(post_pixel_endpoint,post_pixel_data, header_for_all)
+print('reached_pixel')
 print(create_pixel)
+
+#############################  Change Value using put ############################ 
+
+put_graph_endpoint = f'{post_pixel_endpoint}/{post_pixel_data['date']}'
+
+print(put_graph_endpoint)
+
+put_pixel_chnage = {
+    'quantity' : '20.5'
+}
+
+put_pixel = create_user.update_pixel(put_graph_endpoint , put_pixel_chnage , header_for_all)
+print(put_pixel)
+
+#############################  Delete a Pixel ############################ 
+
+delet_graph_endpoint = put_graph_endpoint
+
+Delete_pixel = create_user.Delete_pixel(delet_graph_endpoint , header_for_all)
+print(Delete_pixel)
