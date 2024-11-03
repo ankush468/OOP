15bdacd9659bf2c55b43947b34a8654adbcc02ab
Ankush Rai <ankushrai468@gmail.com>
2024-11-03 08:49:23 +0530

    Track_API_added

--- API-Tracking-System/Track_main.py
+++ API-Tracking-System/Track_main.py
@@ -3,9 +3,13 @@ from datetime import datetime
 from track_API_oop import ISS
 
 
-MY_LAT = 51.507351 # Your latitude
-MY_LONG = -0.127758 # Your longitude
+MY_LAT = 20.593683 # Your latitude
+MY_LONG = 78.962883 # Your longitude
 
+ISS_API = "http://api.open-notify.org/iss-now.json"
+Day_Track_API = "https://api.sunrise-sunset.org/json"
 
 while True:
-    pass
+    Track_ISS_Data = ISS(ISS_API,MY_LAT,MY_LONG)
+    ISS_position = Track_ISS_Data.is_iss_overhead()
+    print(ISS_position)
--- API-Tracking-System/track_API_oop.py
+++ API-Tracking-System/track_API_oop.py
@@ -1,10 +1,27 @@
+from requests import request
+
 class ISS():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,ISS_API:str  ,MY_LAT:float,  MY_LONG:float) -> None:
+
+        self.ISS_API = ISS_API
+        self.MY_LAT = MY_LAT
+        self.MY_LONG = MY_LONG
 
     def is_iss_overhead(self):
-        pass
+
+        response = request(method="GET",url=self.ISS_API)
+        response.raise_for_status()
+        self.data = response.json()
+
+        iss_latitude = float(self.data["iss_position"]["latitude"])
+        iss_longitude = float(self.data["iss_position"]["latitude"])
+
+        if self.MY_LAT-5 <= iss_latitude <= self.MY_LAT+5 and self.MY_LONG-5 <= iss_latitude <= self.MY_LONG+5:
+            return True
+        
+        else:
+            return False
 
     def is_night(self):
         pass
