30ab118f6fd98514bb2d63a987fc7c0907471c40
Ankush Rai <ankushrai468@gmail.com>
2024-11-03 13:54:35 +0530

    addiing pixel

--- Habit_tracing/habit_track_oop.py
+++ Habit_tracing/habit_track_oop.py
@@ -12,7 +12,12 @@ class Habit_tracking():
         response = request(method="POST",url=self.user_creation_url,json=self.user_creation_payload)
         return response.json()
     
-    def Create_graph(self, graph_creation_url, graph_creation_header,graph_creation_payload):
+    def Create_graph(self, graph_creation_url, header_for_all,graph_creation_payload):
 
-        response = request(method="POST",url=graph_creation_url, json=graph_creation_payload, headers=graph_creation_header)
+        response = request(method="POST",url=graph_creation_url, json=graph_creation_payload, headers=header_for_all)
+        return response.json()
+    
+    def Get_graph(self , get_graph_endpoint, header_for_all):
+
+        response = request(method="GET", url=get_graph_endpoint,headers=header_for_all)
         return response.json()
--- Habit_tracing/main.py
+++ Habit_tracing/main.py
@@ -11,11 +11,13 @@ print(HABIT_USER_NAME)
 
 
 
-# create user account
+# create user account return a dict
 
 user_creation_endpoint = '/v1/users'
 
-user_creation_url = BASE_URL+user_creation_endpoint
+user_creation_url = f'{BASE_URL}{user_creation_endpoint}'
+
+print(user_creation_url)
 
 user_creation_payload = {
     'token': HABIT_TRACK_TOKEN,
@@ -25,32 +27,49 @@ user_creation_payload = {
 }
 
 create_user = Habit_tracking(user_creation_url,user_creation_payload)
-user = create_user.Create_user()
-print(user)
+# user = create_user.Create_user()
+# print(user)
 
 # create a graph defination
 
-graph_creation_endpoint = '/v1/users/@'+HABIT_USER_NAME+'graphs'
+graph_creation_endpoint = '/v1/users/'+HABIT_USER_NAME+'/graphs'
 
-graph_creation_url = BASE_URL+graph_creation_endpoint
+graph_creation_url = f'{BASE_URL}{graph_creation_endpoint}'
 
-graph_creation_header = {
+# print(graph_creation_url)
+
+header_for_all = {
     'X-USER-TOKEN' : HABIT_TRACK_TOKEN
 }
 
 graph_creation_payload = {
-    'id':'10',
+    'id':'graph1',
     'name': 'ankush_graph',
-    'unit': 'commit',
-    'type':'int',
+    'unit': 'km',
+    'type':'float',
     'color':'ajisai'
 }
 
-Create_graph = create_user.Create_graph(graph_creation_url, graph_creation_header,graph_creation_payload)
+Create_graph = create_user.Create_graph(graph_creation_url, header_for_all,graph_creation_payload)
 print(Create_graph)
 
 
+# get graph details
+
+get_graph_endpoint = f'{BASE_URL}{graph_creation_endpoint}'
+
+print(get_graph_endpoint)
+
+
+# get_graph = create_user.Get_graph(get_graph_endpoint, header_for_all)
+# print(get_graph)
 
 
 
+# post a value on the graph
 
+post_pixel_endpoint = f'{BASE_URL}{graph_creation_endpoint}/{graph_creation_payload['id']}'
+
+create_pixel = create_user
+
+print(post_pixel_endpoint)
