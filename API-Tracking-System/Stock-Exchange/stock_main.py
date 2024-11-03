5a7c83df2066965af8a2b1940e7e56695afa0527
Ankush Rai <ankushrai468@gmail.com>
2024-11-03 11:46:47 +0530

    STOCK-Done

--- API-Tracking-System/Stock-Exchange/stock_main.py
+++ API-Tracking-System/Stock-Exchange/stock_main.py
@@ -10,8 +10,9 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = os.getenv('STOCK_API_KEY')
-print(STOCK_API_KEY)
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 
+############################## PRAM for STOCK #######################################
 PRAM = {
        'function' : "TIME_SERIES_DAILY",
        'symbol' : STOCK_NAME,    
@@ -22,12 +23,20 @@ PRAM = {
 yesterday = (date.today() - timedelta(days=2)).isoformat()
 day_before_yesterday = (date.today() - timedelta(days=4)).isoformat()
 
-print(yesterday)
-print(day_before_yesterday)
-
-
+# Get stock price from API return string of tuple
 get_stock_price = Stock(STOCK_ENDPOINT,PRAM,yesterday,day_before_yesterday)
 Stock_price = get_stock_price.Stock_price()
-print(Stock_price)
 
+# check the percentage diff between 2 days return float
+diff_percentage = get_stock_price.percentage_difference(Stock_price)
+print(diff_percentage)
+
+############################## PRAM for NEWS #######################################
+
+NEWS_HEADER = {
+       'X-Api-Key': NEWS_API_KEY
+}
+
+if diff_percentage > 2:
+    print('Get News')
 
