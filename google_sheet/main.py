c555669a08f68c39e8cff58027e8e51cc0238d10
Ankush Rai <ankushrai468@gmail.com>
2024-11-03 19:20:22 +0530

    sheety_done

--- google_sheet/main.py
+++ google_sheet/main.py
@@ -2,6 +2,7 @@ from requests import request
 import os
 from datetime import datetime, timedelta, date
 from nutrition_oop import Nutrition
+from pprint import pprint
 
 NUTRITIONIX_API_KEY = os.getenv('NUTRITIONIX_API_KEY')
 NUTRITIONIX_API_ID = os.getenv('NUTRITIONIX_API_ID')
@@ -24,6 +25,36 @@ payload = {
 
 nlp_query = Nutrition(nlp_endpoint, header, payload)
 nlp_response = nlp_query.nlp_exercise()
-print(nlp_response)
+pprint(nlp_response)
 
+ # print day month year
+DATE = datetime.now().strftime("%d/%m/%Y")
 
+# print time in hour min sec format
+TIME = datetime.now().strftime("%X") 
+
+############## GET/UPDATE sheet with the data ##################################
+
+SHEET_URL = 'https://api.sheety.co/c10bc28ab99077d2754b7745ddabadcd/ankush2Workouts/workoutsi'
+
+sheet_headers = {
+    'Authorization': 'Bearer'+' '+NUTRITIONIX_API_KEY
+}
+
+print(sheet_headers)
+
+get_sheet_data = nlp_query.Get_sheet_data(SHEET_URL,sheet_headers )
+print(get_sheet_data)
+
+payload={
+    'workout':{
+        'date': DATE,
+        'time' : TIME,
+        'exercise':nlp_response['exercises'][0]['name'],
+        'duration': nlp_response['exercises'][0]['duration_min'],
+        'calories': nlp_response['exercises'][0]['nf_calories'],
+    }
+}
+
+update_sheet_data = nlp_query.Update_sheet_data(SHEET_URL,sheet_headers,payload)
+print(update_sheet_data)
--- google_sheet/nutrition_oop.py
+++ google_sheet/nutrition_oop.py
@@ -13,3 +13,13 @@ class Nutrition():
 
         response = request(method="POST",url = self.nlp_endpoint, headers = self.header, json = self.payload)
         return response.json()
+    
+    def Get_sheet_data(self, SHEET_URL, sheet_headers):
+
+        response = request(method="GET", headers=sheet_headers, url=SHEET_URL)
+        return response.json()
+    
+    def Update_sheet_data(self, SHEET_URL,sheet_headers,payload):
+
+        response = request(method="POST", headers=sheet_headers, url=SHEET_URL ,json=payload)
+        return response.json()
